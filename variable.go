7ba41c04bd0a443a1d17fecd9323bea05652adf7
Priyanshu Rajput <priyanshu.21march@gmail.com>
2023-07-05 18:20:04 +0530

    Create variable.go

---
+++ variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	fmt.Println(a)
+
+	var c, b = 10, 20
+
+	fmt.Println(c)
+	fmt.Println(b)
+
+	i := 90
+	fmt.Println(i)
+
+	j := "apple"
+	fmt.Println(j)
+
+}
