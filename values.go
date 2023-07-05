30070c409f6324a4e5dcaa1f827d33896456f06b
Priyanshu Rajput <priyanshu.21march@gmail.com>
2023-07-05 18:13:12 +0530

    Create values.go

---
+++ values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("GO" + " Language") //string
+	fmt.Println("7+7=", 7+7)        //integer
+	fmt.Println("7.0/3.0", 7.0/3.0) //float
+	fmt.Println(true && false)      //and operator
+	fmt.Println(true || false)      //or operator
+	fmt.Println(!true)              //not operator
+
+}
